6c2b76a48ec37f7e837f6019e7976a4a569c3350
aysenurcaglar <aysenurcaglar24@gmail.com>
2024-02-08 22:50:56 +0300

    typo

--- cypress/e2e/registerForm.cy.js
+++ cypress/e2e/registerForm.cy.js
@@ -55,12 +55,12 @@ describe("registration", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173");
   });
-  
+
   it("butona tıklanınca form sıfırlanıyor ve user id gösteriliyor", () => {
-    cy.get('input[name="firstName"]').clear().type("Ayşe Nur");
-    cy.get('input[name="lastName"]').clear().type("Çağlar");
-    cy.get('input[name="email"]').clear().type("admin@test.com");
-    cy.get('input[name="password"]').clear().type("123456Qa!");
+    cy.get('input[name="firstName"]').type("Ayşe Nur");
+    cy.get('input[name="lastName"]').type("Çağlar");
+    cy.get('input[name="email"]').type("admin@test.com");
+    cy.get('input[name="password"]').type("123456Qa!");
 
     cy.get("button").click();
 
